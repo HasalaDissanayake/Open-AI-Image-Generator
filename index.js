2af43c527b1d374a317227c8a0e651d0e12e508e
Hasala <hasaladissanayake@yahoo.com>
2023-02-14 18:17:27 +0530

    tried out a tutorial to create a OPEN AI image generator

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Used cors to make connection routes
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
